a009b02beabe9ba5c357e02eb6604bb804eea3f8
Daniel Susetio <dsusetio87@gmail.com>
2024-02-18 16:32:47 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-primary mb-3">
+    <div class="container">
+      <a class="navbar-brand" href="/">WikiCountries</a>
+    </div>
+  </nav>
+  );
+}
 
 export default Navbar;
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,11 @@
-function CountryDetails() {}
+function CountryDetails() {
+
+    return (
+        <div>
+          <h1>Country Detailse</h1>
+    
+        </div>
+      );
+}
 
 export default CountryDetails;
